f174e954961e082cf999071d11a062e9a52b6a82
suneettipirneni <suneettipirneni@icloud.com>
2023-06-02 23:19:38 -0400

    fix dark mode divider colors

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,26 +2,46 @@ import { BlogEntry } from "@/components/BlogEntry";
 import { Title } from "@/components/Title";
 import { allPosts } from "contentlayer/generated";
 
+function PostSection({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={`gap-x-5 gap-y-7 md:gap-y-10 md:divide-none divide-y divide-gray-400 dark:divide-gray-400/50 ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
+
 export default function AboutPage() {
-  const featuredPosts = allPosts.filter((post) => post.featured);
+  const sortedPosts = allPosts.sort((a, b) => {
+    return new Date(b.datetime).getTime() - new Date(a.datetime).getTime();
+  });
+
+  const featuredPosts = sortedPosts.filter((post) => post.featured);
 
   return (
     <div className="flex flex-col w-full md:pb-10 space-y-7 py-7">
       <Title>Featured</Title>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-5 gap-y-7 md:gap-y-10 md:divide-none divide-y divide-gray-400">
+      <PostSection className="grid grid-cols-1 md:grid-cols-2">
         {featuredPosts.map((entry) => (
           <BlogEntry key={entry.title} post={entry} />
         ))}
-      </div>
+      </PostSection>
 
       <Title>Other Posts</Title>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-7 md:gap-y-7 md:divide-none divide-y divide-gray-400">
-        {allPosts
+      <PostSection className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {sortedPosts
           .filter((entry) => !entry.featured)
           .map((entry) => (
             <BlogEntry key={entry.title} post={entry} />
           ))}
-      </div>
+      </PostSection>
     </div>
   );
 }
