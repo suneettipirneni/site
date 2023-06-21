e8cfd836148c5a152aa4306afe0a5fd9c4e6d16e
suneettipirneni <suneettipirneni@icloud.com>
2023-06-21 18:38:32 -0400

    fix sitemap for blog posts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,25 @@
 import type { MetadataRoute } from 'next'
+import { allPosts } from 'contentlayer/generated';
+
+const BASE_URL = 'https://www.suneettipirneni.stream'
  
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.suneettipirneni.stream',
+      url: BASE_URL,
       lastModified: new Date(),
     },
     {
-      url: 'https://www.suneettipirneni.stream/projects',
+      url: `${BASE_URL}/projects`,
       lastModified: new Date(),
     },
     {
-      url: 'https://www.suneettipirneni.stream/blog',
+      url: `${BASE_URL}/blog`,
       lastModified: new Date(),
     },
+    ...allPosts.map((post) => ({
+      url: `${BASE_URL}/blog/posts/${post.slug}`,
+      lastModified: new Date(),
+    }))
   ]
 }
