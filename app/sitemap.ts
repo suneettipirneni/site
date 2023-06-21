61a90bd0291cc434831e540ec903db57f3961728
suneettipirneni <suneettipirneni@icloud.com>
2023-06-21 16:06:20 -0400

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://acme.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://acme.com/projects',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://acme.com/blog',
+      lastModified: new Date(),
+    },
+  ]
+}
