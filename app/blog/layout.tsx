99f951114b6970af54e8787f617575cf0013531b
suneettipirneni <suneettipirneni@icloud.com>
2023-07-21 15:00:23 -0400

    adjust gradients a bit in light mode

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,16 +1,24 @@
 import { PropsWithChildren } from "react";
 import Image from "next/image";
 import background from "../../public/thunk.png";
+import backgroundDark from "../../public/thunk-dark.png";
 
 export default function BlogLayout({ children }: PropsWithChildren) {
 	return (
 		<div className="flex w-full justify-center">
-			<Image
-				src={background}
-				alt=""
-				placeholder="blur"
-				className="absolute inset-0 top-0 -z-10  h-[400px] w-screen overflow-clip object-fill md:h-[700px]"
-			/>
+			<picture>
+				<source
+					srcSet={backgroundDark.src}
+					media="(prefers-color-scheme: dark)"
+				/>
+				<Image
+					src={background}
+					alt=""
+					placeholder="blur"
+					className="absolute inset-0 top-0 -z-10  h-[400px] w-screen overflow-clip object-fill md:h-[700px]"
+				/>
+			</picture>
+
 			{children}
 		</div>
 	);
