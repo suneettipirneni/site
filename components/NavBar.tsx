8ab8519470b539d337bd558e9f90de98ab4664a0
suneettipirneni <suneettipirneni@icloud.com>
2023-07-21 19:46:14 -0400

    hide nav background on scroll

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import NextLink from "next/link";
-import type { ReactNode } from "react";
+import { useEffect, type ReactNode, useState } from "react";
 import { useSelectedLayoutSegment } from "next/navigation";
 import Image from "next/image";
 import profilePic from "@/public/me.png";
@@ -36,13 +36,38 @@ export interface NavBarProps {
 
 export function NavBar({ tabs }: NavBarProps) {
 	const segment = useSelectedLayoutSegment() ?? "home";
+	const [shouldShowBackground, setShouldShowBackground] = useState(false);
+
+	const handleScroll = (e: Event) => {
+		if (window.scrollY > 50) {
+			setShouldShowBackground(true);
+			return;
+		}
+
+		setShouldShowBackground(false);
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+	}, []);
 
 	// Determines if a tab is currently active based on the current path.
 	const isActive = (tab: string) => tab.replaceAll("/", "") === segment;
 
+	const withBackgroundStyle =
+		"bg-white/80 dark:bg-black/50 dark:ring-gray-400/20 ring-gray-200/75 ring-1 backdrop-blur-xl lg:shadow-lg lg:shadow-gray-200/30";
+
 	return (
-		<div className="sticky top-0 z-50 flex h-[50px] w-full flex-row items-center justify-center border-b  border-gray-200 dark:border-gray-200/5 lg:h-nav lg:border-none">
-			<div className="flex h-full w-[calc(850px_+_50px)] max-w-full flex-row items-center justify-between bg-white/80 p-3 bg-blend-saturation ring-1 ring-gray-200/75 backdrop-blur-xl dark:bg-black/50 dark:ring-gray-400/20 lg:h-[60px] lg:rounded-full lg:shadow-lg lg:shadow-gray-200/30 lg:dark:border-gray-200/25 lg:dark:shadow-none">
+		<div
+			className={`sticky top-0 z-50 flex h-[50px] w-full flex-row items-center justify-center ${
+				shouldShowBackground && "border-b"
+			} border-gray-200 p-1 dark:border-gray-200/5 lg:h-nav lg:border-none`}
+		>
+			<div
+				className={`flex h-full w-[calc(850px_+_50px)] max-w-full flex-row items-center justify-between ${
+					shouldShowBackground ? withBackgroundStyle : "bg-transparent"
+				} p-3 bg-blend-saturation transition-colors lg:h-[60px] lg:rounded-full  lg:dark:border-gray-200/25 lg:dark:shadow-none`}
+			>
 				<NextLink href="/" className="flex flex-row items-center">
 					<Image
 						src={profilePic}
@@ -53,21 +78,19 @@ export function NavBar({ tabs }: NavBarProps) {
 					/>
 				</NextLink>
 
-				<div className="flex max-h-[40px] flex-row rounded-full bg-gray-200/50 p-1 dark:bg-white/5">
+				<div className="flex max-h-[40px] flex-row p-1">
 					{tabs.map((tab, index) => (
 						<NextLink
 							key={tab.name}
-							className={`relative z-10 flex flex-row items-center px-4 py-1 text-sm font-medium transition ${
-								isActive(tab.href)
-									? "text-black dark:text-white"
-									: "text-black dark:text-white"
+							className={`relative z-10 flex flex-row items-center px-4 py-1 text-sm font-medium transition-all ${
+								isActive(tab.href) && "text-white dark:text-black"
 							}`}
 							href={`/${tab.href}`}
 						>
 							{isActive(tab.href) && (
 								<span
 									key={`outer-${tab.name}`}
-									className="absolute inset-0 -z-10 rounded-full bg-white shadow-sm dark:bg-white/20"
+									className="absolute inset-0 -z-10 rounded-full bg-black/90 text-white dark:bg-white"
 								/>
 							)}
 
