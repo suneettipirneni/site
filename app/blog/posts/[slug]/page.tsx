76988c60be30fae62719a0e73fb8b5f559310adf
suneettipirneni <suneettipirneni@icloud.com>
2023-08-18 14:31:03 -0400

    consistent image borders

--- app/blog/posts/[slug]/page.tsx
+++ app/blog/posts/[slug]/page.tsx
@@ -82,7 +82,7 @@ export default function Post({ params }: { params: { slug: string } }) {
 					height={600}
 					placeholder="blur"
 					blurDataURL={BLUR_DATA_URL}
-					className="mt-5 aspect-[1200_/_600] w-full rounded-3xl object-cover"
+					className="mt-5 aspect-[1200_/_600] w-full rounded-xl object-cover"
 				/>
 			</h1>
 			<div className="col-start-2 row-start-3 mb-2 min-w-0 max-w-postcontent self-start">
