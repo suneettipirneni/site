a75a66db41b9787af7a135620b461170269c2d0f
suneettipirneni <suneettipirneni@icloud.com>
2025-10-27 21:39:01 -0400

    update to nextjs 16

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import unusedImports from "eslint-plugin-unused-imports";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig([{
+    extends: [...nextCoreWebVitals],
+
+    plugins: {
+        "unused-imports": unusedImports,
+    },
+
+    rules: {
+        "unused-imports/no-unused-imports": "error",
+    },
+}]);
